0bf032d5690d92f35647d6b5c8317fb4ec20289c
0marA <theengineermachine@gmail.com>
2022-08-05 00:55:15 -0500

    Unzipping folders and printing SVG paths

--- walk.py
+++ walk.py
@@ -1,8 +1,16 @@
 import os
+import zipfile
 
-if __name__ == "__main__":
-    for (root,dirs,files) in os.walk('.', topdown=True):
-        print (root)
-        print (dirs)
-        print (files)
-        print ('--------------------------------')
+def unzip(path_to_zip_file, directory_to_extract_to):
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(directory_to_extract_to)
+
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in files:
+        if name.endswith(".zip"):
+            unzip(os.path.join(root, name), os.path.join(root, name[:-4]))
+            os.remove(os.path.join(root, name))
+        if name.endswith(".svg"):
+            print("SVG: " + os.path.join(root, name))
+            
+        
