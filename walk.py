e11d189b484a2e49dd499c768397eaad75ab9ec7
0marA <theengineermachine@gmail.com>
2022-08-05 00:35:59 -0500

    Initial Commit

---
+++ walk.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == "__main__":
+    for (root,dirs,files) in os.walk('.', topdown=True):
+        print (root)
+        print (dirs)
+        print (files)
+        print ('--------------------------------')
