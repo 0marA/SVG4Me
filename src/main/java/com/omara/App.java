aa3189a7b1830bbcf75407d37ce13a09f590af0f
0marA <theengineermachine@gmail.com>
2022-08-06 13:28:51 -0500

    Praise to Jack 🙏 🙏 🙏 Font no longer alien

--- src/main/java/com/omara/App.java
+++ src/main/java/com/omara/App.java
@@ -7,41 +7,27 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
+    private static Font arial;
     private static Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("SVG 4 Me");
         button = new Button();
-        Font arial = new Font("arial", 20);
+        arial = new Font("arial", 20);
         button.setFont(arial);
-
         button.setText("Switch to Secondary");
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
 
-        // scene = new Scene(loadFXML("primary"), 640, 480);
         scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    // static void setRoot(String fxml) throws IOException {
-    // scene.setRoot(loadFXML(fxml));
-    // }
-
-    // private static Parent loadFXML(String fxml) throws IOException {
-    // FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml +
-    // ".fxml"));
-    // return fxmlLoader.load();
-    // }
 
     public static void main(String[] args) {
         launch(args);
