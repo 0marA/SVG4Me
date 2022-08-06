c8acaf36babc8962c0e8f34843d919b8f9fa1c48
0marA <theengineermachine@gmail.com>
2022-08-06 10:34:16 -0500

    Praise to Jack 🙏 🙏 🙏 Font no longer alien

--- src/main/java/com/omara/App.java
+++ src/main/java/com/omara/App.java
@@ -1,12 +1,11 @@
 package com.omara;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -14,25 +13,38 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Button button;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("SVG 4 Me");
+        button = new Button();
+        Font arial = new Font("arial", 20);
+        button.setFont(arial);
+
+        button.setText("Switch to Secondary");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        // scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+    // static void setRoot(String fxml) throws IOException {
+    // scene.setRoot(loadFXML(fxml));
+    // }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+    // private static Parent loadFXML(String fxml) throws IOException {
+    // FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml +
+    // ".fxml"));
+    // return fxmlLoader.load();
+    // }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
--- src/main/java/com/omara/PrimaryController.java
+++ src/main/java/com/omara/PrimaryController.java
@@ -5,8 +5,8 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    // @FXML
+    // private void switchToSecondary() throws IOException {
+    // App.setRoot("secondary");
+    // }
 }
