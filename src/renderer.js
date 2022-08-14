6dc4c5dc90103de228b5bb5da8aea7e987176f81
0marA <theengineermachine@gmail.com>
2022-08-14 16:25:07 -0500

    Properly sized SVGs

--- src/renderer.js
+++ src/renderer.js
@@ -8,13 +8,17 @@ btn.addEventListener("click", async () => {
     mainFolderPath = await window.electronAPI.openFolder();
     folderPathElement.innerText += mainFolderPath;
     SVGPaths = await window.electronAPI.getSVGPaths();
-    SVGPathElement.innerText = SVGPaths;
+    //SVGPathElement.innerText = SVGPaths;
 
     for (let i = 0; i < SVGPaths.length; i++) {
-        console.log("Created an image");
         let img = document.createElement("img");
+        let path = document.createElement("h3");
         img.src = SVGPaths[i];
+        img.width = 200;
+        img.height = 200;
+        path.innerText = SVGPaths[i];
         document.getElementById("body").appendChild(img);
+        document.getElementById("body").appendChild(path);
     }
 });
 
